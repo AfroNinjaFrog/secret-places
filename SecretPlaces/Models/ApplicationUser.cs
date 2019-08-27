44747454404957f0316292d422c82b57ca82144c
saharashwal <54313508+saharashwal@users.noreply.github.com>
2019-08-27 15:32:27 +0300

    fix first and last name bug

--- SecretPlaces/Models/ApplicationUser.cs
+++ SecretPlaces/Models/ApplicationUser.cs
@@ -8,6 +8,8 @@ namespace SecretPlaces.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public bool IsAdmin { get; set; }
     }
 }
